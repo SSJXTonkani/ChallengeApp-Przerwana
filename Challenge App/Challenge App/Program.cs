34afa2eebd47583ecb93d9724349a0e5c132c704
SSJXTonkani <dawidsochax@gmail.com>
2023-10-13 11:55:28 +0200

    Zadanie z dnia 4

---
+++ Challenge App/Challenge App/Program.cs
@@ -0,0 +1,16 @@
+﻿var name = "Ewa";
+var gender = "Kobieta";
+var age = 33;
+
+if (gender == "kobieta" && age < 30)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (gender == "mężczyzna" && age < 18)
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+}
