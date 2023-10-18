29e40b91166981d4525473b3e9e67d925a898dab
SSJXTonkani <dawidsochax@gmail.com>
2023-10-18 16:49:09 +0200

    Day 6 rework

--- Challenge App/Challenge App/Program.cs
+++ Challenge App/Challenge App/Program.cs
@@ -7,10 +7,10 @@ Employee employee3 = new Employee("Kamila", "Kowalska", 19);
 
 
 
-employee1.AddScore(9);
+employee1.AddScore(19);
 employee1.AddScore(8);
 employee1.AddScore(4);
-employee1.AddScore(2);
+employee1.AddScore(123);
 employee1.AddScore(4);
 
 employee2.AddScore(2);
@@ -27,18 +27,19 @@ employee3.AddScore(16);
 
 List<Employee> employees = new List<Employee>()
 {
-    employee1,employee2,employee3
+    employee1,employee2,employee3,
 
 };
 
 int maxResult = -1;
 Employee employeeWithMaxResult = null;
 
-foreach (var employee in employees)
+foreach (Employee employee in employees)
 {
     if (employee.Result > maxResult)
     {
         employeeWithMaxResult = employee;
+        maxResult = employee.Result;
     }
 }
 Console.WriteLine($"pracownica: {employeeWithMaxResult.Name} {employeeWithMaxResult.Surname} lat: {employeeWithMaxResult.Age} Osiągnęła największy wynik: {employeeWithMaxResult.Result}");
