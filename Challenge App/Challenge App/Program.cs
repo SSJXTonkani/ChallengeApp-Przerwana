5b71b8b545c7971e84b2c15b83bbd78d01127a0b
SSJXTonkani <dawidsochax@gmail.com>
2023-10-18 10:51:28 +0200

    Day 6 Homework

--- Challenge App/Challenge App/Program.cs
+++ Challenge App/Challenge App/Program.cs
@@ -1,78 +1,44 @@
-﻿internal class Program
+﻿using Challenge_App;
+
+
+Employee employee1 = new Employee("Paulina", "Kowalska", 22);
+Employee employee2 = new Employee("Wiktoria", "Kowalska", 23);
+Employee employee3 = new Employee("Kamila", "Kowalska", 19);
+
+
+
+employee1.AddScore(9);
+employee1.AddScore(8);
+employee1.AddScore(4);
+employee1.AddScore(2);
+employee1.AddScore(4);
+
+employee2.AddScore(2);
+employee2.AddScore(5);
+employee2.AddScore(6);
+employee2.AddScore(1);
+employee2.AddScore(2);
+
+employee3.AddScore(10);
+employee3.AddScore(11);
+employee3.AddScore(12);
+employee3.AddScore(10);
+employee3.AddScore(16);
+
+List<Employee> employees = new List<Employee>()
 {
-    private static void Main(string[] args)
+    employee1,employee2,employee3
+
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
     {
-        int number = int.MaxValue-1;
-        string numberAsString = number.ToString();
-        char[] letters = numberAsString.ToArray();
-
-        int counter0 = 0;
-        int counter1 = 0;
-        int counter2 = 0;
-        int counter3 = 0;
-        int counter4 = 0;
-        int counter5 = 0;
-        int counter6 = 0;
-        int counter7 = 0;
-        int counter8 = 0;
-        int counter9 = 0;
-
-        foreach (char letter in letters)
-        {
-            if (letter == '0')
-            {
-                counter0++;
-            }
-            else if (letter == '1')
-            {
-                counter1++;
-            }
-            else if (letter == '2')
-            {
-                counter2++;
-            }
-            else if (letter == '3')
-            {
-                counter3++;
-            }
-            else if (letter == '4')
-            {
-                counter4++;
-            }
-            else if (letter == '5')
-            {
-                counter5++;
-            }
-            else if (letter == '6')
-            {
-                counter6++;
-            }
-            else if (letter == '7')
-            {
-                counter7++;
-            }
-            else if (letter == '8')
-            {
-                counter8++;
-            }
-            else if (letter == '9')
-            {
-                counter9++;
-            }
-
-        }
-        {
-            Console.WriteLine("wyniki dla liczby:" + number);
-            Console.WriteLine("0 = " + counter0);
-            Console.WriteLine("1 = " + counter1);
-            Console.WriteLine("2 = " + counter2);
-            Console.WriteLine("3 = " + counter3);
-            Console.WriteLine("4 = " + counter4);
-            Console.WriteLine("5 = " + counter5);
-            Console.WriteLine("6 = " + counter6);
-            Console.WriteLine("7 = " + counter7);
-            Console.WriteLine("8 = " + counter8);
-            Console.WriteLine("9 = " + counter9);
-        }
+        employeeWithMaxResult = employee;
     }
 }
+Console.WriteLine($"pracownica: {employeeWithMaxResult.Name} {employeeWithMaxResult.Surname} lat: {employeeWithMaxResult.Age} Osiągnęła największy wynik: {employeeWithMaxResult.Result}");
